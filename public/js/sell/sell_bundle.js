b685fe2e0e36ea2f623760e363f6ca6778381027
Max Metral <mmetral@paypal.com>
2014-12-19 18:24:57 -0500

    Compartmentalize the main bundle on the sell page into a class to allow cleaner component access

--- public/js/sell/sell_bundle.js
+++ public/js/sell/sell_bundle.js
@@ -1,21 +1,22 @@
-window['initialize_appsforhere_sell'] = function (options) {
+window['PayPalHerePOS'] = function PayPalHerePOSfn(options) {
 
     options = $.extend({}, options);
 
     accounting.settings.currency.symbol = _currency.symbol;
 
-    var storage = new (require('./storage'))(_email.replace('.', ''));
-    var locationManager = new (require('./location'))();
-    var invoiceManager = new (require('./invoiceManager'))(locationManager);
-    var orderManager = new (require('./savedOrders'))(locationManager, invoiceManager);
-    var checkin = new (require('./checkin'))(locationManager, invoiceManager);
-    var hardware = options.hardware === false ? null : new (require('./hardware'))(locationManager);
-    var serverSocket = new (require('./serverSocket'))(hardware);
-    var catalog = new (require('./catalog'))(invoiceManager);
-    var payment = new (require('./payment'))(locationManager, invoiceManager, checkin);
-    var cart = new (require('./cart'))(invoiceManager, checkin, payment);
-    var customerInfo = new (require('./customerInfo'))(invoiceManager, cart, checkin, locationManager);
-    var receipt = new (require('./receipt'))(invoiceManager);
+    this.storage = new (require('./storage'))(_email.replace('.', ''));
+    this.locationManager = new (require('./location'))();
+    this.invoiceManager = new (require('./invoiceManager'))(this.locationManager);
+    this.orderManager = new (require('./savedOrders'))(this.locationManager, this.invoiceManager);
+    this.checkin = new (require('./checkin'))(this.locationManager, this.invoiceManager);
+    this.hardware = options.hardware === false ? null : new (require('./hardware'))(this.locationManager);
+    this.serverSocket = new (require('./serverSocket'))(this.hardware);
+    this.catalog = new (require('./catalog'))(this.invoiceManager);
+    this.payment = new (require('./payment'))(this.locationManager, this.invoiceManager, this.checkin);
+    this.cart = new (require('./cart'))(this.invoiceManager, this.checkin, this.payment);
+    this.customerInfo = new (require('./customerInfo'))(this.invoiceManager, this.cart, this.checkin, this.locationManager);
+    this.receipt = new (require('./receipt'))(this.invoiceManager);
+    var self = this;
 
 // TODO this was basically completely refactored from a really long "procedural" javascript
 // implementation. So there are still all sorts of artifacts of that in the object structures.
@@ -23,62 +24,62 @@ window['initialize_appsforhere_sell'] = function (options) {
 // and add more event triggering, etc. But don't think that this structure is the "intent,"
 // it's just a simple division of a big ball of code.
     $(function () {
-        locationManager.getBrowserLocation();
+        self.locationManager.getBrowserLocation();
         [
-            invoiceManager,
-            cart,
-            locationManager,
-            hardware,
-            catalog,
-            orderManager,
-            serverSocket,
-            checkin,
-            payment,
-            customerInfo,
-            receipt
+            self.invoiceManager,
+            self.cart,
+            self.locationManager,
+            self.hardware,
+            self.catalog,
+            self.orderManager,
+            self.serverSocket,
+            self.checkin,
+            self.payment,
+            self.customerInfo,
+            self.receipt
         ].forEach(function (m) {
                 if (m) {
-                    m.setup(storage);
+                    m.setup(self.storage);
                 }
             });
 
         // Wire up event routing
 
         // When there is a hardware device selected, we need the web socket up
-        if (hardware) {
-            hardware.on('changed', function (e, device) {
-                serverSocket.attach(device);
-                serverSocket.connect();
+        if (self.hardware) {
+            self.hardware.on('changed', function (e, device) {
+                self.serverSocket.attach(device);
+                self.serverSocket.connect();
             });
         }
 
         // Let the server know about order changes when the socket is up
-        cart.on('render', function () {
-            serverSocket.sendIfOpen('activeOrder', {inv: this.invoiceManager.deepFreeze(), tot: tots});
+        self.cart.on('render', function (e,info) {
+            self.serverSocket.sendIfOpen('activeOrder', {inv: self.invoiceManager.deepFreeze(), tot: info.totals});
         });
 
         // When we're ready to take payment
-        cart.on('selectingPaymentType', function (e, totals) {
-            if (hardware && hardware.device) {
-                serverSocket.subscribe(totals.total.toString());
+        self.cart.on('selectingPaymentType', function (e, totals) {
+            if (self.hardware && self.hardware.device) {
+                self.serverSocket.subscribe(totals.total.toString());
             }
         });
 
-        payment.on('hidden', function (e) {
-            serverSocket.unsubscribe();
+        self.payment.on('hidden', function (e) {
+            self.serverSocket.unsubscribe();
         });
 
-        locationManager.on('selected', function () {
-            if (hardware) {
-                hardware.load();
+        self.locationManager.on('selected', function () {
+            if (self.hardware) {
+                self.hardware.load();
             }
-            checkin.ensurePolling();
-            orderManager.update();
+            self.checkin.ensurePolling();
+            self.orderManager.update();
         });
 
-        serverSocket.on('deviceEvent', function (e, args) {
+        self.serverSocket.on('deviceEvent', function (e, args) {
             if (args.type === 'Swipe') {
-                payment.swipeDetected(args.data);
+                self.payment.swipeDetected(args.data);
             }
         });
 
